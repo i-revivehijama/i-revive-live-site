71fbd6545458ea338b70aecd7772a92c00f916fa
irevivehijama <irevivehijama@gmail.com>
2025-03-24 21:37:52 +0530

    dr derails

--- src/app/components/LevelTwo.tsx
+++ src/app/components/LevelTwo.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React from "react";
 
 const LevelTwo = () => {
@@ -69,7 +68,6 @@ const LevelTwo = () => {
         </div>
 
         {/* Right Side: Video */}
-        <div>
         <div className="aspect-video">
           <iframe
             src="https://www.youtube.com/embed/8hjUOqhcVd8"
@@ -77,14 +75,6 @@ const LevelTwo = () => {
             allowFullScreen
             className="w-full h-full rounded-lg shadow-lg"
           ></iframe>
-        </div>
-         <Image
-                  alt="class"
-                  src="/class1.jpg" // Update with the actual image path
-                  width={500}
-                  height={500}
-                  className="w-full h-full max-h-[750px] object-cover rounded-lg"
-                />
         </div>
       </div>
 
